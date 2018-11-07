89293f60ad4fcb9ea1fde8ffff292f8331748383
Jinlin Song <jlsong@ucdavis.edu>
2018-11-06 22:48:09 -0800

    upload final version

--- src/rolling_median.py
+++ src/rolling_median.py
@@ -1,24 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  6 20:50:31 2018
+
+@author: jinlin.song
+
+Python version: Python 3.6.1
+"""
+
 import json
-import os
 from datetime import datetime
 from collections import defaultdict
 import sys
 epoch = datetime.utcfromtimestamp(0)
 
 def read_json(file_path):
+    '''
+    Read all venmo payments from the given file. If the record is invalid, it will skip.
+    '''
     data = []
     with open(file_path, 'r') as f:
         for line in f:
             try:
                 data.append(json.loads(line))
             except:
-                print("Error: Not able to load json.")
+                print("Error: Not able to load json record, skip bad data.")
+    f.close()
     return data
 
-path = r"C:\Users\jinlin.song\Desktop\Jinlin\coding-challenge\insight_testsuite\tests\mytest_1\venmo_input\venmo-trans.txt"
-data = read_json(path)
-
 class transact:
+    '''
+    Convert venmo payment to python class transact.
+    Each transact includes actor, target, and created_time_sec, the second differences from datetime.datetime(1970, 1, 1, 0, 0).
+
+    Method is_valid will return False if the payment is not valid, such as missing data, actor is the same with target,
+    and payment format is invalid.
+    '''
     def __init__(self, json):
         self.value = True
         try:
@@ -29,27 +45,41 @@ class transact:
             print("Error: Json format is invalid.")
             self.value = False
         if self.value:
+            ### mising actor or target
             if not self.actor or not self.target:
                 print("Error: Actor or target is null.")
                 self.value = False
+            ### actor is the same with target
             elif self.actor == self.target:
                 print("Error: Actor is the same with target.")
                 self.value = False
+
+    ### payment validation
     def is_valid(self):
         return self.value
 
-for e in data:
-    print(transact(e).is_valid())
-
 class Graph:
+    '''
+    Define payment network as python class graph.
+    For each payment, it only stores the lastest undirected payment created_time in graph, type defaultdict(defaultdict(int)).
+    For each actor or target, it stores the number of total distinct connections in graph_degree, type defaultdict(int).
+
+    Method add_transact to add new payment.
+    Method del_payment to delete payment. 
+    '''
     ### Initialize Graph
     def __init__(self):
         self.graph = defaultdict(lambda: defaultdict(int))
         self.graph_degree = defaultdict(int)
         
-        
-    ### add edge
+    ### method add_transact, add edge
     def add_transact(self, transact):
+        '''
+        Add new payment to payment network.
+        If it is a new payment, add it to the graph, and update graph_degree.
+        If it is a duplicated payment, with new created_time, update graph with the lastest created_time.
+        If it is a old payment, skip.
+        '''
         ### transact validation
         if transact.is_valid():
             ### created_time window validation
@@ -66,8 +96,12 @@ class Graph:
         else:
             return False
     
-    ### remove edge            
+    ### method del_payment, remove edge            
     def del_payment(self, actor, target):
+        '''
+        Remove undirected payment from graph. Delete record from graph, and update graph_degree. If graph_degree is 0, remove it.
+        Since it is undirected, we remove edge, and update graph_degree from both sides.
+        '''
         try:
             del self.graph[actor][target]
             if len(self.graph[actor]) == 0:
@@ -89,157 +123,102 @@ class Graph:
         except:
             return False
 
-g = Graph()
-
-for record in data:
-    g.add_transact(transact(record))
-
-g.graph_degree
-g.graph
-
-### calculate median_degree
 def median_degree(Graph):
+    '''
+    Calculate median_degree for given Graph, return float.
+    '''
     degree_list = list(Graph.graph_degree.values())
     degree_list = list(filter(lambda e: e != 0, degree_list))
+
     if not degree_list:
         return None
+
+    ### sort the Non-zero degree_list
     degree_list.sort()
     len_list = len(degree_list)
     middle_pos = len_list//2
+
     if len_list%2 == 0:
         return float((degree_list[middle_pos - 1] + degree_list[middle_pos])/2.0)
     else:
         return float(degree_list[middle_pos])
 
-median_degree(Graph())
-median_degree(g)
+class process_data_stream:
+    '''
+    Class for processing venmo transacts.
+    payments_window is 60 seconds.
+    Process transact_record one by one, writing median value to given output file.
+    '''
+    def __init__(self, input_path,output_path):
+        ### Default parameters, payments_window
+        self.payments_window = 60
+        
+        ### Initialize parameters
+        self.latest_transact = 0
+        self.median_value = None
+        
+        ### Initialize Graph
+        self.g = Graph()
 
-def write_median(file_path, value):
-    with open(file_path, 'a') as f:
-       f.write("{:.2f}".format(value) + '\n')
-    f.close()
+        self.input_path = input_path
+        self.output_f = open(output_path, 'w')
 
-def process_data_stream():
-    payments_window = 60
-    latest_transact = 0
-    median_value = None
-    g = Graph()
-    
-    if len(sys.argv) != 2:
-        print('Please specify two parameters: input path and output path')
+        ### load all venmo payments to json dataset
+        self.json_dataset = read_json(input_path)
+
+
+    def process_data(self, transact_record):
+        '''
+        Method to process one transact_record.
+        '''
+        if transact_record.is_valid():
+            if transact_record.created_time_sec <= (self.latest_transact - self.payments_window):
+                ### If the transact_record is out of payments_window, output the previous median value 
+                self.output_f.write("{:.2f}".format(self.median_value) + '\n')
+            else:
+                ### Remove edges beyond payments_window
+                if transact_record.created_time_sec > self.latest_transact:
+                    self.latest_transact = transact_record.created_time_sec
+                    for i in list(self.g.graph):
+                        for j in list(self.g.graph[i]):
+                            ### Remove payments which are prior to payments_window.
+                            ### Since it is undirected, when we delete graph edge graph[i][j], and it will remove graph[j][i] at the same time   
+                            if self.g.graph[i][j] <= (self.latest_transact - self.payments_window) and i < j:
+                                self.g.del_payment(i, j)
+
+                ### add latest payment
+                self.g.add_transact(transact_record)
+
+                ### update median
+                self.median_value = median_degree(self.g)
+
+                ### output median
+                self.output_f.write("{:.2f}".format(self.median_value) + '\n')
+        else:
+            pass
+
+    def close_file(self):
+        self.output_f.close()
+
+def main():
+    ### get input path and out_put path
+    if len(sys.argv) != 3:
+        print('Please specify three parameters: rolling_median.py, input path and output path!')
         return False
     else:    
         input_path = sys.argv[1]
-        output_f = open(sys.argv[2], 'w')
-    
-    json_dataset = read_json(input_path)
-    
-    for json_record in json_dataset:
-        transact_record = transact(json_record)    
-        if transact_record.created_time_sec <= (latest_transact - payments_window):
-            output_f.write("{:.2f}".format(median_value) + '\n')
-            return True
-        else:
-            if transact_record.created_time_sec > latest_transact:
-                latest_transact = transact_record.created_time_sec
-                for i in g.graph:
-                    for j in list(g.graph[i]):
-                        if g.graph[i][j] <= (latest_transact - payments_window) and i < j:
-                            g.del_payment(i, j)
-            g.add_transact(transact_record)
-            median_value = median_degree(g)
-            output_f.write("{:.2f}".format(median_value) + '\n')
+        output_path = sys.argv[2]
+
+    process_data_stream_var = process_data_stream(input_path, output_path)
+
+    ### Process venmo payment in json dataset
+    for json_record in process_data_stream_var.json_dataset:
+        transact_record = transact(json_record)
+        process_data_stream_var.process_data(transact_record)
+
+    process_data_stream_var.close_file()
     return True
 
-
-    
-        
-
-
-
-
-
-###############################      ###############################
-############################### main ###############################
-###############################      ###############################
-input_path = r"C:\Users\jinlin.song\Desktop\Jinlin\coding-challenge\venmo_input\input_test.txt"
-input_path1 = r"C:\Users\jinlin.song\Desktop\Jinlin\coding-challenge\data-gen\venmo-trans_abd.txt"
-input_path2 = r"C:\Users\jinlin.song\Desktop\Jinlin\coding-challenge\data-gen\venmo-trans.txt"
-
-output_path = r"C:\Users\jinlin.song\Desktop\Jinlin\coding-challenge\venmo_output\output_test.txt"
-
-try:
-    os.remove(output_path)
-except OSError:
-    pass
-
-data1 = read_json(input_path1)
-data2 = read_json(input_path2)
-
-process_ = process()
-for json_ in data:
-    transact_point = transact(json_)
-    if transact_point.is_valid():
-        median_point = process_.process_transact(transact_point)
-        write_median(output_path, median_point)
-    else:
-        pass
-
-process_.g.graph_degree
-    
-transact_point.created_time_sec
-process_.latest_transact
-
-def process():
-    '''
-    Method for processing payment records with moving window of 1 minute.
-    simulates real time streaming by reading records one by one using python
-    generator object to read lines from file.
-    '''
-    ### Initialize Graph
-    g = Graph()
-    ### Initialize activeNodes at current time
-    activeRecords = []
-    ### Initialize max_ts older date..
-    max_ts = dateutil.parser.parse('1989-07-10T13:20:02Z')
-    ### Process payment records....
-    if len(sys.argv) == 0:
-        print('Specify input and output arguments')    
-    else:    
-        f = open(sys.argv[1], 'r')
-        wf = open(sys.argv[2], 'w')
-        
-    for data in f:
-                payment = json.loads(data)
-                ts = dateutil.parser.parse(payment['created_time'])
-                ### Keep track of current max processing record
-                if ts > max_ts:
-                    max_ts = ts
-                ### Update Graph based on latest payment record
-                if ts >= (max_ts - timedelta(seconds=60)):
-                    activeRecords.append((payment['actor'],payment['target'],ts))
-                    ### Remove edges based on expiry of 1 minute windows
-                    edgesToRemove = invalidateEdges(activeRecords, max_ts)
-                    if len(edgesToRemove) >= 1:
-                        g.removeEdges(edgesToRemove)
-                        #### Update activeRecord list by removing the edges...
-                        activeRecords = \
-                           [record for record in activeRecords \
-                              if (record[0],record[1]) not in edgesToRemove]
-                    ### Update latest payment
-                    g.addVertex(payment['actor'])
-                    g.addEdge(payment['actor'], payment['target'])
-                    writeOutput(median(g.getVerticesDegrees()), wf)
-                else:
-                    ### Write out median even in cases of out of order record
-                    writeOutput(median(g.getVerticesDegrees()), wf)
-
 if __name__ == '__main__':
-    process()
-    
-
-if __name__ == '__main__':
-    process()
-    
-
+    main()
 
